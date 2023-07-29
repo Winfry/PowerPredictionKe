096273b5c905d9ecf8b9dcc2acdb6553ce9ced42
winfry77 <Winfrynyarangi@gmail.com>
2023-07-29 23:25:04 +0300

    PowerPredictionKe

--- RabaiPower.py
+++ RabaiPower.py
@@ -10,9 +10,31 @@ from PIL import Image
 
 from plotly import graph_objs as go
 
-import fbprophet
-from fbprophet import Prophet
+import prophet
+from prophet import Prophet
 #from prophet import Prophet 
-from fbprophet.plot import plot_plotly
-
+from prophet.plot import plot_plotly
 import base64
+
+sum=0
+sum100=0
+sum200=0
+sum300=0
+sum600=0
+sum900=0
+i=0
+cost=0
+cost100=0
+cost200=0
+cost300=0
+cost600=0
+cost900=0
+
+
+#st.title("ELECTRICITY CONSUMPTION")
+st.write("""
+# ELECTRICITY CONSUMPTION PREDICTION WEB APP ⚡️
+
+This app predicts the **energy consumption for the next month**!📈  
+Download a sample dataset here if you don't have one:""")
+
